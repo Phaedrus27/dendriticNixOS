11249044b337789462ad3be80a5c5bf852abccb2
Phaedrus27 <Phaedrus27@proton.me>
2026-05-02 23:08:18 +0200

    more wg changes

--- modules/Arr/protonvpn.nix
+++ modules/Arr/protonvpn.nix
@@ -27,16 +27,26 @@
     Type = "oneshot";
     RemainAfterExit = true;
     ExecStart = pkgs.writeShellScript "wg-vpn-start" ''
-      # Copy config to temp file
-      install -m 600 ${config.sops.secrets.protonvpn_wireguard.path} /tmp/wg-vpn.conf
-      # Bring up WireGuard in the namespace
-      ${pkgs.iproute2}/bin/ip netns exec vpn \
-        ${pkgs.wireguard-tools}/bin/wg-quick up /tmp/wg-vpn.conf
-    '';
-    ExecStop = pkgs.writeShellScript "wg-vpn-stop" ''
-      ${pkgs.iproute2}/bin/ip netns exec vpn \
-        ${pkgs.wireguard-tools}/bin/wg-quick down /tmp/wg-vpn.conf
-    '';
+  # Create WireGuard interface in host namespace
+  ${pkgs.iproute2}/bin/ip link add wg-vpn type wireguard
+  # Move it into the vpn namespace
+  ${pkgs.iproute2}/bin/ip link set wg-vpn netns vpn
+  # Apply config inside namespace
+  ${pkgs.iproute2}/bin/ip netns exec vpn \
+    ${pkgs.wireguard-tools}/bin/wg setconf wg-vpn \
+    ${config.sops.secrets.protonvpn_wireguard.path}
+  # Add IP address inside namespace
+  ${pkgs.iproute2}/bin/ip -n vpn addr add 10.2.0.2/32 dev wg-vpn
+  # Bring interface up
+  ${pkgs.iproute2}/bin/ip -n vpn link set wg-vpn up
+  # Add default route through WireGuard
+  ${pkgs.iproute2}/bin/ip -n vpn route add default dev wg-vpn
+  # Bring up loopback
+  ${pkgs.iproute2}/bin/ip -n vpn link set lo up
+'';
+ExecStop = pkgs.writeShellScript "wg-vpn-stop" ''
+  ${pkgs.iproute2}/bin/ip -n vpn link del wg-vpn
+'';
   };
 };
 
