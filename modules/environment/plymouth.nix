411c888d96f75ba61d660e74453022fed67bfd60
Phaedrus27 <Phaedrus27@proton.me>
2026-05-02 09:24:15 +0200

    Tweaks for plymouth

--- modules/environment/plymouth.nix
+++ modules/environment/plymouth.nix
@@ -1,13 +1,7 @@
 { self, inputs, ... }: {
-  flake.nixosModules.plymouth = { lib, config, ... }: {
-    options.modules.plymouth = {
-      enable = lib.mkEnableOption "plymouth boot splash";
-    };
-
-    config = lib.mkIf config.modules.plymouth.enable {
-      boot.plymouth.enable = true;
-      boot.consoleLogLevel = 0;
-      boot.kernelParams = [ "quiet" "splash" "udev.log_level=0" ];
-    };
+  flake.nixosModules.plymouth = { lib, ... }: {
+    boot.plymouth.enable = true;
+    boot.consoleLogLevel = 0;
+    boot.kernelParams = [ "quiet" "splash" "udev.log_level=0" ];
   };
 }
