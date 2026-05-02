2707fc7ff5bd4ee94fd68b8b272d6a2b47a9b413
Phaedrus27 <Phaedrus27@proton.me>
2026-05-02 23:05:58 +0200

    wireguard changes

--- modules/Arr/protonvpn.nix
+++ modules/Arr/protonvpn.nix
@@ -19,27 +19,26 @@
 
     # WireGuard interface inside the namespace
     systemd.services.wg-vpn = {
-      description = "ProtonVPN WireGuard in VPN namespace";
-      after = [ "netns-vpn.service" ];
-      requires = [ "netns-vpn.service" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = pkgs.writeShellScript "wg-vpn-start" ''
-          ${pkgs.iproute2}/bin/ip netns exec vpn ${pkgs.iproute2}/bin/ip link set lo up
-          ${pkgs.iproute2}/bin/ip link add wg-vpn type wireguard
-          ${pkgs.iproute2}/bin/ip link set wg-vpn netns vpn
-          ${pkgs.wireguard-tools}/bin/wg setconf wg-vpn ${config.sops.secrets.protonvpn_wireguard.path}
-          ${pkgs.iproute2}/bin/ip -n vpn addr add $(${pkgs.wireguard-tools}/bin/wg show wg-vpn | grep "interface" -A5 | grep "address" | awk '{print $2}') dev wg-vpn
-          ${pkgs.iproute2}/bin/ip -n vpn link set wg-vpn up
-          ${pkgs.iproute2}/bin/ip -n vpn route add default dev wg-vpn
-        '';
-        ExecStop = pkgs.writeShellScript "wg-vpn-stop" ''
-          ${pkgs.iproute2}/bin/ip -n vpn link del wg-vpn
-        '';
-      };
-    };
+  description = "ProtonVPN WireGuard in VPN namespace";
+  after = [ "netns-vpn.service" ];
+  requires = [ "netns-vpn.service" ];
+  wantedBy = [ "multi-user.target" ];
+  serviceConfig = {
+    Type = "oneshot";
+    RemainAfterExit = true;
+    ExecStart = pkgs.writeShellScript "wg-vpn-start" ''
+      # Copy config to temp file
+      install -m 600 ${config.sops.secrets.protonvpn_wireguard.path} /tmp/wg-vpn.conf
+      # Bring up WireGuard in the namespace
+      ${pkgs.iproute2}/bin/ip netns exec vpn \
+        ${pkgs.wireguard-tools}/bin/wg-quick up /tmp/wg-vpn.conf
+    '';
+    ExecStop = pkgs.writeShellScript "wg-vpn-stop" ''
+      ${pkgs.iproute2}/bin/ip netns exec vpn \
+        ${pkgs.wireguard-tools}/bin/wg-quick down /tmp/wg-vpn.conf
+    '';
+  };
+};
 
     environment.systemPackages = with pkgs; [
       wireguard-tools
