459d2e170d591efa4734c7ed86b78dd710dddd33
Phaedrus27 <Phaedrus27@proton.me>
2026-05-02 20:11:12 +0200

    typo

--- modules/features/Yubikey.nix
+++ modules/features/Yubikey.nix
@@ -7,7 +7,7 @@
     # GPG agent with pinentry for YubiKey PIN prompts
     programs.gnupg.agent = {
       enable = true;
-      enable SSHSupport = true;
+      enableSSHSupport = true;
       pinentryPackage = pkgs.pinentry-gtk2;
     };
 
