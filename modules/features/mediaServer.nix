ead7e51537f11280a3dd8f17b172354146cd1525
Phaedrus27 <Phaedrus27@proton.me>
2026-05-10 18:01:17 +0200

    dnsmasq for forwarding to router

--- modules/features/mediaServer.nix
+++ modules/features/mediaServer.nix
@@ -7,5 +7,16 @@
       self.nixosModules.jellyfin
       self.nixosModules.caddy
     ];
+    services.dnsmasq = {
+      enable = true;
+      settings = {
+        server = [ "192.168.1.1" ];
+        listen-address = [ "127.0.0.1" "100.85.58.101" ];
+        bind-interfaces = true;
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [ 53 ];
+    networking.firewall.allowedUDPPorts = [ 53 ];
   };
 }
