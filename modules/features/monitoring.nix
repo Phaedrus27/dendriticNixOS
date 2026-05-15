b285ca75b6fb79c14511baeb2d6096d6e8bbe0ed
Phaedrus27 <Phaedrus27@proton.me>
2026-05-15 11:29:28 +0200

    bugsplat

--- modules/features/monitoring.nix
+++ modules/features/monitoring.nix
@@ -4,14 +4,12 @@
     services.smartd = {
       enable = true;
       autodetect = true;
-      extraOptions = [
-        "-M exec ${pkgs.writeShellScript "smartd-discord-alert" ''
-          WEBHOOK=$(cat ${config.sops.secrets.discord_webhook.path})
-          curl -s -X POST "$WEBHOOK" \
-            -H "Content-Type: application/json" \
-            -d "{\"content\": \"⚠️ **SMART Alert on squirtle**: $SMARTD_MESSAGE\"}"
-        ''}"
-      ];
+      defaults = "-a -o on -S on -n standby,q -s (S/../.././02|L/../../6/03) -W 4,45,55 -m root -M exec ${pkgs.writeShellScript "smartd-discord-alert" ''
+        WEBHOOK=$(cat ${config.sops.secrets.discord_webhook.path})
+        curl -s -X POST "$WEBHOOK" \
+          -H "Content-Type: application/json" \
+          -d "{\"content\": \"⚠️ **SMART Alert on squirtle**: $SMARTD_MESSAGE\"}"
+      ''}";
     };
   };
 }
