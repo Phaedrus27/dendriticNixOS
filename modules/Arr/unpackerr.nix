aca89bccee190476bad8a38b6e56f06ecc6617ec
Phaedrus27 <Phaedrus27@proton.me>
2026-05-22 11:52:05 +0200

    changed to systemd approach

--- modules/Arr/unpackerr.nix
+++ modules/Arr/unpackerr.nix
@@ -4,22 +4,31 @@
     sops.secrets.sonarr_api_key = {};
     sops.secrets.radarr_api_key = {};
 
-    services.unpackerr = {
-      enable = true;
-      settings = {
-        sonarr = [{
-          url = "http://localhost:8989";
-          api_key = "filepath:${config.sops.secrets.sonarr_api_key.path}";
-          paths = [ "/mnt/storage/downloads" ];
-          protocols = "torrent,TorrentDownloadProtocol";
-        }];
-        radarr = [{
-          url = "http://localhost:7878";
-          api_key = "filepath:${config.sops.secrets.radarr_api_key.path}";
-          paths = [ "/mnt/storage/downloads" ];
-          protocols = "torrent,TorrentDownloadProtocol";
-        }];
+    systemd.services.unpackerr = {
+      description = "Unpackerr - Extract downloads for Sonarr and Radarr";
+      after = [ "network.target" "sonarr.service" "radarr.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "simple";
+        User = "unpackerr";
+        Group = "media";
+        ExecStart = pkgs.writeShellScript "unpackerr-start" ''
+          ${pkgs.unpackerr}/bin/unpackerr \
+            --sonarr.url=http://localhost:8989 \
+            --sonarr.api_key="$(cat ${config.sops.secrets.sonarr_api_key.path})" \
+            --sonarr.paths=/mnt/storage/downloads \
+            --radarr.url=http://localhost:7878 \
+            --radarr.api_key="$(cat ${config.sops.secrets.radarr_api_key.path})" \
+            --radarr.paths=/mnt/storage/downloads
+        '';
+        Restart = "on-failure";
       };
     };
+
+    users.users.unpackerr = {
+      isSystemUser = true;
+      group = "media";
+    };
+
   };
 }
