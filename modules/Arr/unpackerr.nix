d4193e18d8e44643f63ce304ccbff95c30ffd501
Phaedrus27 <Phaedrus27@proton.me>
2026-05-22 11:54:27 +0200

    splat

--- modules/Arr/unpackerr.nix
+++ modules/Arr/unpackerr.nix
@@ -1,33 +1,37 @@
 { self, inputs, ... }: {
   flake.nixosModules.unpackerr = { config, pkgs, ... }: {
 
-    sops.secrets.sonarr_api_key = {};
-    sops.secrets.radarr_api_key = {};
+    sops.secrets.sonarr_api_key = { owner = "unpackerr"; };
+    sops.secrets.radarr_api_key = { owner = "unpackerr"; };
+
+    users.users.unpackerr = {
+      isSystemUser = true;
+      group = "media";
+    };
 
     systemd.services.unpackerr = {
       description = "Unpackerr - Extract downloads for Sonarr and Radarr";
       after = [ "network.target" "sonarr.service" "radarr.service" ];
       wantedBy = [ "multi-user.target" ];
+      environment = {
+        UN_SONARR_0_URL = "http://localhost:8989";
+        UN_SONARR_0_PATHS_0 = "/mnt/storage/downloads";
+        UN_SONARR_0_PROTOCOLS = "torrent,TorrentDownloadProtocol";
+        UN_RADARR_0_URL = "http://localhost:7878";
+        UN_RADARR_0_PATHS_0 = "/mnt/storage/downloads";
+        UN_RADARR_0_PROTOCOLS = "torrent,TorrentDownloadProtocol";
+      };
       serviceConfig = {
         Type = "simple";
         User = "unpackerr";
         Group = "media";
-        ExecStart = pkgs.writeShellScript "unpackerr-start" ''
-          ${pkgs.unpackerr}/bin/unpackerr \
-            --sonarr.url=http://localhost:8989 \
-            --sonarr.api_key="$(cat ${config.sops.secrets.sonarr_api_key.path})" \
-            --sonarr.paths=/mnt/storage/downloads \
-            --radarr.url=http://localhost:7878 \
-            --radarr.api_key="$(cat ${config.sops.secrets.radarr_api_key.path})" \
-            --radarr.paths=/mnt/storage/downloads
-        '';
         Restart = "on-failure";
+        ExecStart = pkgs.writeShellScript "unpackerr-start" ''
+          export UN_SONARR_0_API_KEY=$(cat ${config.sops.secrets.sonarr_api_key.path})
+          export UN_RADARR_0_API_KEY=$(cat ${config.sops.secrets.radarr_api_key.path})
+          exec ${pkgs.unpackerr}/bin/unpackerr
+        '';
       };
-    };
-
-    users.users.unpackerr = {
-      isSystemUser = true;
-      group = "media";
     };
 
   };
