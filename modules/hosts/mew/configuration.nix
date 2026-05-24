3ff968aeb19f2615feb133050c2d5ea96b6d9dca
Phaedrus27 <Phaedrus27@proton.me>
2026-05-24 13:45:32 +0200

    splat

--- modules/hosts/mew/configuration.nix
+++ modules/hosts/mew/configuration.nix
@@ -30,9 +30,6 @@
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    # Enable networking
-    networking.networkmanager.enable = true;
-
     # Enable flakes
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -117,7 +114,6 @@
     vesktop
     vlc
     pywalfox-native
-    obsidian
     ];
 
     nix.gc = {
