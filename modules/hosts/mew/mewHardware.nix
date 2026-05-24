8dd216e052a8d7d79c441e52e0ce806f144002a8
Phaedrus27 <Phaedrus27@proton.me>
2026-05-24 21:32:42 +0200

    added available kernel modules

--- modules/hosts/mew/mewHardware.nix
+++ modules/hosts/mew/mewHardware.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.mewHardware = { config, lib, pkgs, modulesPath, ... }: {
     imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" "usbhid" "hid_generic" ];
     boot.initrd.kernelModules = [ ];
     boot.kernelModules = [ "kvm-amd" ];
     boot.extraModulePackages = [ ];
