3a6cedccb192a8814e44d1dfa2270149e672f9e4
Phaedrus27 <Phaedrus27@proton.me>
2026-05-24 14:00:14 +0200

    added u2f mappings

--- modules/hosts/mew/mewSops.nix
+++ modules/hosts/mew/mewSops.nix
@@ -4,6 +4,10 @@
       defaultSopsFile = "${self}/secrets/secrets.yaml";
       age.plugins = [ pkgs.age-plugin-yubikey ];
       secrets.tailscale_authkey = {};
+      secrets.u2f_mappings = {
+        path = "/etc/u2f_mappings";
+        mode = "0444";
+      };
     };
   };
 }
