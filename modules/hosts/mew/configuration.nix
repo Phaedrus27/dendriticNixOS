cdde9db74a39945fe1178d95b4891c2ec2261a48
Phaedrus27 <Phaedrus27@proton.me>
2026-05-31 19:59:57 +0200

    removed fixed ip for mew

--- modules/hosts/mew/configuration.nix
+++ modules/hosts/mew/configuration.nix
@@ -16,12 +16,6 @@
 
     networking = {
       hostName = "mew";
-      interfaces.wlp1s0.ipv4.addresses = [{
-        address = "192.168.1.151";
-        prefixLength = 24;
-      }];
-      defaultGateway = "192.168.1.1";
-      nameservers = [ "1.1.1.1" "8.8.8.8" ];
     };
 
     time.timeZone = "Europe/Brussels";
