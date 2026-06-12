ee0a9e1cba0dacdd5525ceb45f027566f6c962ed
Phaedrus27 <Phaedrus27@proton.me>
2026-06-12 17:53:25 +0200

    File moves for refactor

--- modules/media/mediaServer.nix
+++ modules/media/mediaServer.nix
@@ -1,13 +1,11 @@
 { self, inputs, ... }: {
   flake.nixosModules.mediaServer = { ... }: {
     imports = [
-      self.nixosModules.protonvpn
-      self.nixosModules.qbittorrent
       self.nixosModules.arr
       self.nixosModules.jellyfin
       self.nixosModules.caddy
       self.nixosModules.monitoring
-      self.nixosModules.unpackerr
+      self.nixosModules.seedbox
     ];
 
     services.dnsmasq = {
---
+++ modules/media/seedbox.nix
@@ -0,0 +1,11 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.seedbox = { ... }: {
+    # VPN-confined download pipeline: wireguard netns, torrent client
+    # inside it, archive extraction feeding the *arrs.
+    imports = [
+      self.nixosModules.protonvpn
+      self.nixosModules.qbittorrent
+      self.nixosModules.unpackerr
+    ];
+  };
+}
