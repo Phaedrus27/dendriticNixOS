d492c9021348572207ac36d3e66c769d4a3d6e96
Phaedrus27 <Phaedrus27@proton.me>
2026-06-13 10:52:17 +0200

    Removed pam.u2f from charizardsecurity

--- modules/hosts/charizard/charizardSecurity.nix
+++ modules/hosts/charizard/charizardSecurity.nix
@@ -20,11 +20,5 @@
     environment.sessionVariables = {
       SSH_AUTH_SOCK = "/run/user/1000/gnupg/S.gpg-agent.ssh";
     };
-
-    # pam_u2f — YubiKey touch for sudo
-    # Generated with: pamu2fcfg > /tmp/u2f && pamu2fcfg -n >> /tmp/u2f (swap keys between commands)
-    security.pam.u2f.settings.authfile = pkgs.writeText "u2f_mappings" ''
-      phaedrus:bQGIdVctyjerQS1Wzv8APAItVDoI6BY+l2gFJ9sdsGb5+KrV8MQyiMcuqHxkdq698HTySteDq3J+aMg18P21fg==,rakcy2MsQK9TqTHsWxXsg/0th31SlzzG34KZRChDXioO2EddQeNngExrXMtWD3G8ErQkByTz40rrPxD3TWjfmw==,es256,+presence:grSTD34kpGp0s86ctats3Ax86TKFj9bHlEZcEIDUWBshQPIH/+JjaQdIg3A59HqLnDVkh1O/UtPVkN45rt4njw==,vNpjvNCAfc25gbOQfBkf6NXtevXfrXHMUXGP+BXtdqq2JdxO89mnaH/JVbNa4g5cNudeznglwEOeFxWIpwKOPg==,es256,+presence
-    '';
   };
 }
