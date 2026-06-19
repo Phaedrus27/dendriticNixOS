c138e63793847142ded2f0d79b3f913c3422247f
Phaedrus27 <Phaedrus27@proton.me>
2026-06-19 22:27:58 +0200

    Corrected sops path in Squirtle.

--- modules/hosts/squirtle/squirtleSops.nix
+++ modules/hosts/squirtle/squirtleSops.nix
@@ -1,7 +1,7 @@
 { self, inputs, ... }: {
   flake.nixosModules.squirtleSops = { ... }: {
     sops = {
-      defaultSopsFile = "${self}/secrets/secrets.yaml";
+      defaultSopsFile = "${self}/modules/secrets/secrets.yaml";
       age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     };
   };
