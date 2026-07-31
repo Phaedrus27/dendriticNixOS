090d25348a02827985064c0f850ac7a0de280334
Phaedrus27 <Phaedrus27@proton.me>
2026-07-31 18:15:30 +0200

    added phaedrus to trusted users in base.nix

--- modules/features/base.nix
+++ modules/features/base.nix
@@ -35,6 +35,8 @@
       KbdInteractiveAuthentication = false;
     };
 
+    nix.settings.trusted-users = ["root" "phaedrus" ];
+
     users.users.phaedrus.openssh.authorizedKeys.keys = [
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJtarCyjvvCxzi1PwWavZXaPLcHRiDeIAZr2tyAFA+zXAAAADHNzaDp5dWJpa2V5QQ== yubikeyA"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOpNEHSKkHZCiCkuss0aNrLFKet3gEkQbWfysFzpgI+bAAAADHNzaDp5dWJpa2V5Qw== yubikeyC"
