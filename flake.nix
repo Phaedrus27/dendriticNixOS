4d906fb7256073df7a44955ff90a1d36160dfe4c
Phaedrus27 <Phaedrus27@proton.me>
2026-09-18 10:42:29 +0200

    Splat

--- flake.nix
+++ flake.nix
@@ -35,3 +35,15 @@
     # pins, and overriding it risks a Pi that won't boot.
     nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/v1.20260517.0";
   };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake
+    { inherit inputs; }
+    (inputs.import-tree ./modules);
+
+  nixConfig = {
+    extra-substituters = [ "https://nixos-raspberrypi.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
+  };
+}
